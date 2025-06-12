706e4a6995049203e2c550a308617f39860848b2
gaurav0973 <codejourneypanda@gmail.com>
2025-06-12 16:27:27 +0530

    logout is being implimened

---
+++ src/app/api/users/logout/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+export async function GET() {
+  try {
+    // Clear the session cookie
+    const response = NextResponse.json({ message: "Logged out successfully" });
+    response.cookies.set("token", "", {
+      httpOnly: true,
+        expires: new Date(0), // Set the cookie to expire immediately
+    });
+    return response;
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: error.message || "Error in logging out the user" },
+      { status: 500 }
+    );
+  }
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,32 @@
+"use client";
+import React from "react";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 function ProfilePage() {
+
+    const router = useRouter();
+
+    const handleLogout = async () => {
+        try {
+            const response = await axios.get("/api/users/logout");
+            console.log("Logout response:", response.data);
+            router.push("/login");
+
+        } catch (error: any) {
+            console.error("Error during logout:", error.message);
+        }
+    };
+
+
   return (
-    <div>ProfilePage</div>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-3xl font-bold text-center text-purple-700 mb-6">Profile Page</h1>
+        <p className="text-center text-gray-600">This is the profile page where user details will be displayed.</p>
+        <button onClick={handleLogout} className="mt-4 bg-purple-600 hover:bg-purple-700 text-white font-semibold py-2 px-4 rounded-lg transition duration-300">
+            Logout
+        </button>
+    </div>
   )
 }
 export default ProfilePage
